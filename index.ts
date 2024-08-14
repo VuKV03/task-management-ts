b6e0ef2e40aa9c4799aabe5e695f56d8bec0919d
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-14 15:51:44 +0700

    build: cors

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import * as database from "./config/database";
 import mainV1Routes from "./api/v1/routes/index.route";
 import bodyParser from "body-parser";
+import cors from "cors"
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+app.use(cors());
 
 // parse application/json
 app.use(bodyParser.json());
